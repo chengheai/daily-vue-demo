292ae5a6c94e0d96f8a5bd391d0cfd3e8959e185
chengheai <842385139@qq.com>
2019-08-01 22:06:33 +0800

    add switch

--- src/components/Heart.vue
+++ src/components/Heart.vue
@@ -1,13 +1,35 @@
 <template>
-  <h1>hhhhhhhhhhhhhh</h1>
+  <div>
+    <toggle-button @change="onChangeEventHandler" />
+
+    <toggle-button v-model="myDataVariable" />
+    {{myDataVariable}}
+    <toggle-button :value="false" color="#82C7EB" :sync="true" :labels="true" />
+
+    <toggle-button :value="true" :labels="{checked: 'Foo', unchecked: 'Bar'}" />
+    <toggle-button color='red' switch-color='yellow' :width='200' :height='20' :value="true" :labels="{checked: 'Foo', unchecked: 'Bar'}" />
+    <toggle-button :color="{checked: '#00FF00', unchecked: '#FF0000', disabled: '#CCCCCC'}" :width='100' :height='30' :value="true" :labels="{checked: 'Foo', unchecked: 'Bar'}" />
+  </div>
 </template>
 
 <script>
+import ToggleButton from './../pages/switch.vue';
 export default {
-
-}
+  components: {
+    'toggle-button': ToggleButton
+  },
+  data() {
+    return {
+      myDataVariable: ''
+    };
+  },
+  methods: {
+    onChangeEventHandler(val) {
+      console.log(val);
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
