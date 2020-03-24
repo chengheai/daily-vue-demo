0404acc69b06a7f177d8029aa277a04a0dbbf059
chengheai <842385139@qq.com>
2020-03-24 22:43:32 +0800

    ndsfnsfnsfnsfdnsakf
    Signed-off-by: chengheai <842385139@qq.com>

---
+++ src/components/phoneinput.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div @focusout="inputBlur" @focusin="inputFocus">
+      <input type="tel" placeholder="请输入手机号" />
+      <input type="number" placeholder="请输入验证码" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      timer: null,
+    };
+  },
+  methods: {
+    inputBlur(e) {
+      if (e && e.target && e.target.tagName && e.target.tagName.toLowerCase() === 'input') {
+        this.timer = setTimeout(() => {
+          window.scrollTo(0, 0);
+        }, 0);
+      }
+    },
+    inputFocus(e) {
+      if (e && e.target && e.target.tagName && e.target.tagName.toLowerCase() === 'input') {
+        clearTimeout(this.timer);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
